53cb11c527b0b2952fd2f8cacec19f8592693d94
chenjiaxin@touchdata.io <chenjiaxin@touchdata.io>
2020-06-30 16:43:32 +0800

    c

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,33 +1,46 @@
 <template>
     <div class='header'>
-        <div class='header-left'>返回</div>
-        <div class='header-input'>输入城市/景点/游玩主题</div>
-        <div class='header-right'>城市</div>
+        <div class='header-left'>
+            <div class="iconfont back-icon">&#xe624;</div>
+        </div>
+        <div class='header-input'>
+            <span class='iconfont'>&#xe632;</span>
+            输入城市/景点/游玩主题
+        </div>
+        <div class='header-right'>
+            城市
+            <span class='iconfont arrow-icon'>&#xe64a;</span>
+        </div>
     </div>
 </template>
 
 <script>
-export default {
+export default {   
     name: 'HomeHeader',
 }
 </script>
 
-<style lang='stylus' scoped>
+<style lang='stylus' scoped> 
+    @import '~styles/varibles.styl';
     .header 
         display: flex
         line-height: .86rem
-        background: #00bcd4
+        background: $bgColor
         color: #fff
         .header-left
             width: .64rem
             float: left
             text-align: center
+            .back-icon
+                text-align: center
+                font-size: .4rem
         .header-input
             flex: 1
             height: .64rem
             line-height: .64rem
             margin-top: .12rem
             margin-left: .2rem
+            padding-left: .2rem
             background: #fff
             border-radius: .1rem
             color: #ccc
@@ -35,5 +48,8 @@ export default {
             width: 1.24rem
             float: right 
             text-align: center
+            .arrow-icon
+                font-size: .24rem
+                margin-left: -.04rem
 
 </style>
